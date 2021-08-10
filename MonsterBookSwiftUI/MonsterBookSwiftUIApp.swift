cbde5d3e0a274387fe8e0d4ef96578f99a773d47
Handy <handy.kwok@gmail.com>
2021-08-10 14:46:35 +0700

    Initial Commit

---
+++ MonsterBookSwiftUI/MonsterBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterBookSwiftUIApp.swift
+//  MonsterBookSwiftUI
+//
+//  Created by Handy Handy on 10/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MonsterBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
